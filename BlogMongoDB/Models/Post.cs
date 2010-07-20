ca2a0c229c0023498733eeee76e80d19b56051e1
joshcoffman <joshcoffman@gmail.com>
2010-07-20 07:48:19 -0700

    adding Post model to MongoDB demo

---
+++ BlogMongoDB/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogMongoDB.Models
+{
+    public class Post
+    {
+        public virtual string Id { get; set; }
+        public virtual string Hash { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Content { get; set; }
+        public virtual DateTime Published { get; set; }
+        public virtual DateTime Created { get; set; }
+    }
+}
