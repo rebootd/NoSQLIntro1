da21a9ecf3e786c27eabe4c6f2a32eb2b48da6be
joshcoffman <joshcoffman@gmail.com>
2010-08-31 23:24:31 -0700

    a rough perf test

--- BlogRavenDB/Tests/PostTests.cs
+++ BlogRavenDB/Tests/PostTests.cs
@@ -20,5 +20,31 @@ namespace BlogRavenDB.Tests
                 posts = postsQuery.ToList();
             }
         }
+
+        [Fact]
+        public void do_perf_inserts()
+        {
+            int count = 100000;
+            DateTime start = DateTime.Now;
+            var session = DocumentSession;
+            for (int loop = 1; loop <= count; loop++)
+            {
+                Post post = new Post
+                {
+                    Content = "test content",
+                    Hash = "perf-test",
+                    Title = "perf test",
+                    Created = DateTime.Now,
+                    Published = DateTime.Now.AddYears(1000)
+                };
+                if (post.Tags == null) post.Tags = new List<Tag>();
+                post.Tags.Add(new Tag { Name = "perf" });
+                session.Store(post);
+            }
+            DocumentSession.SaveChanges();
+            TimeSpan span = DateTime.Now - start;
+            System.Diagnostics.Debug.WriteLine("raven insert span: " + span.TotalMilliseconds.ToString());
+            System.Diagnostics.Debug.WriteLine("rows/sec: " + (count / span.TotalSeconds).ToString());
+        }
     }
 }
