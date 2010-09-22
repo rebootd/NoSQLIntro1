92d194efb00153bcb1b35e5bcfd4f36485460d27
joshcoffman <joshcoffman@gmail.com>
2010-09-21 21:16:25 -0700

    forgot to add PostsTextSearch.cs

---
+++ BlogRavenDB/Indexes/PostsTextSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Raven.Client.Indexes;
+using Raven.Database.Indexing;
+using BlogRavenDB.Models;
+
+namespace BlogRavenDB.Indexes
+{
+    //public class PostsTextSearch : AbstractIndexCreationTask
+    //{
+    //    public override IndexDefinition CreateIndexDefinition()
+    //    {
+    //        return new IndexDefinition<Post, Post>
+    //        {
+    //            Map = posts => from post in posts
+    //                           select new { post },
+    //            Indexes = { { x => x., FieldIndexing.NotAnalyzed } }
+    //        }
+    //        .ToIndexDefinition(DocumentStore.Conventions); 
+    //    }
+    //}
+}
